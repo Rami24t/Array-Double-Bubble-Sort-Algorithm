6193d7557e4a241dea4bab4081108321e5a66bee
Rami <103028944+Rami24t@users.noreply.github.com>
2023-08-26 16:51:29 +0200

    Update newBubbleSort.js
    optimize

--- newBubbleSort.js
+++ newBubbleSort.js
@@ -1,21 +1,31 @@
 function ramiBubbleSort(arr) {
   const array = [...arr];
   let temp = 0;
+  let keepGoing1 = true;
+  let keepGoing2 = true;
+  let swapped = false;
+
   for (let x = 0; x <= array.length / 4; x++) {
-    for (let i = x; i < array.length - 1; i++) {
+    for (let i = x; i < array.length - 1 && keepGoing1; i++) {
       if (array[i] > array[i + 1]) {
         temp = array[i];
         array[i] = array[i + 1];
         array[i + 1] = temp;
+        swapped = true
       }
     }
-    for (let i = array.length - 1 - x - 1; i > 0; i--) {
+    keepGoing1 = swapped;
+    swapped = false
+    for (let i = array.length - 1 - x - 1; i > 0 && keepGoing2; i--) {
       if (array[i] < array[i - 1]) {
         temp = array[i];
         array[i] = array[i - 1];
         array[i - 1] = temp;
+        swapped = true
       }
     }
+    keepGoing2 = swapped;
+    swapped = false;
   }
   return array;
   // Only change code above this line
